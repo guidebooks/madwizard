677dc3018bb4998c7c196ff49f1b33e9a183a9c6
Nick Mitchell <nickm@us.ibm.com>
2022-04-20 09:22:16 -0400

    feat: add `madwizard` bin to package.json

---
+++ bin/madwizard.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node --experimental-specifier-resolution=node
+
+/* eslint-env node */
+/* ^^^ rather than add env: node globally in package.json */
+
+/**
+ * This is a front-end to the CLI module.
+ */
+
+const isNpx = process.env.npm_command === "exec"
+
+import(isNpx ? "madwizard" : "..").then((_) => _.cli(process.argv.slice(1)))
