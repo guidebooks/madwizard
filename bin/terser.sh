82c463851404bbc9001492483434b4127df6ec9b
Nick Mitchell <nickm@us.ibm.com>
2022-05-14 15:24:44 -0400

    chore: use terser to optimize builds
    I don't really get parcel. It seems so buggy. Yet another bug is that it doesn't seem to optimize our builds, despite the docs claiming this is the default for "production" builds (i.e. those outputs of `terser build`).
    
    And if I try to use a `targets`... I can get optimized builds (by explicitly specifying `optimize: true` in the target), but then i don't get a type declaration... sigh.
    
    So this PR uses terser explicitly in our `build` script target.

---
+++ bin/terser.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# TODO windows
+
+for file in ./dist/*.*js; do
+    T=$(mktemp)
+    (npx terser --compress --mangle -- $file > $T && mv $T $file) &
+done
+
+wait
+ls -lh ./dist/*.*js | awk '{printf "%-5s %s", $5, $NF "\n"}'
