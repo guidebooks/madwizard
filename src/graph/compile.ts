fa5c68ef944ceb64bcf00db533126927ab4d1261
Nick Mitchell <nickm@us.ibm.com>
2022-06-01 15:28:11 -0400

    fix: choice expansion should not expand beyond first choice
    This is an optimization, not a functional change in the graph models. Hence no tests should be affected.

--- src/graph/compile.ts
+++ src/graph/compile.ts
@@ -344,6 +344,7 @@ export async function compile(
         ? undefined
         : await doExpand(
             parts.length === 1 ? parts[0] : ordering === "parallel" ? parallel(parts) : sequence(parts),
+            choices,
             options
           )
 
