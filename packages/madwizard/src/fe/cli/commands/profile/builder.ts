a2543f47444686f3a3a1de4716f7309c296a6195
Nick Mitchell <nickm@us.ibm.com>
2023-02-05 10:54:06 -0500

    chore: code cleanup for profile command registration

---
+++ packages/madwizard/src/fe/cli/commands/profile/builder.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Argv } from "yargs"
+
+import Opts from "../../options.js"
+
+type NamedProfileOpts = Opts & {
+  profile: string
+}
+
+type SrcProfileOpts = {
+  srcProfile: string
+}
+
+type TargetProfileOpts = {
+  tgtProfile: string
+}
+
+export function namedProfileBuilder(yargs: Argv<Opts>): Argv<NamedProfileOpts> {
+  return yargs.positional("profile", {
+    type: "string",
+    describe: "Name of a profile",
+  })
+}
+
+export function srcAndTargetNamedProfileBuilder(yargs: Argv<Opts>): Argv<SrcProfileOpts & TargetProfileOpts> {
+  return yargs
+    .positional("srcProfile", {
+      type: "string",
+      describe: "Name of source profile",
+    })
+    .positional("tgtProfile", {
+      type: "string",
+      describe: "Name of target profile",
+    })
+}
---
+++ packages/madwizard/src/fe/cli/commands/profile/index.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CommandModule } from "yargs"
+
+import Opts from "../../options.js"
+import { MadWizardOptions } from "../../../MadWizardOptions.js"
+
+import getProfile from "./get.js"
+import listProfiles from "./list.js"
+import cloneProfile from "./clone.js"
+import deleteProfile from "./delete.js"
+
+export default function profileModule(providedOptions: MadWizardOptions): CommandModule<Opts, Opts> {
+  return {
+    command: "profile <subcommand>",
+    describe: "Commands for view, listing, deleting, and copying profiles",
+    builder: (yargs) =>
+      yargs
+        .command(listProfiles(providedOptions))
+        .command(getProfile(providedOptions))
+        .command(deleteProfile(providedOptions))
+        .command(cloneProfile(providedOptions)),
+    handler: async () => {
+      throw new Error("Missing subcommand")
+    },
+  }
+}
